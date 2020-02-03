c6be6c7f8712c73e16376f96c7b88886b3fd2db6
Anton Chestikov <chestikov.anton@gmail.com>
2020-02-03 22:15:44 +0500

    Added card flip begaviour

--- Assets/Scripts/CardView.cs
+++ Assets/Scripts/CardView.cs
@@ -1,21 +1,31 @@
 ﻿using System;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
 [RequireComponent(typeof(RectTransform))]
-public class CardView : MonoBehaviour
+[RequireComponent(typeof(Animator))]
+public class CardView : MonoBehaviour, IPointerClickHandler
 {
+    [SerializeField] private GameObject _frontSide;
+    [SerializeField] private GameObject _backSide;
     [SerializeField] private Text _numberText;
     [SerializeField] private Text _rewardTypeText;
     [SerializeField] private Text _rewardQuantityType;
     [SerializeField] private CardScriptableObject _card;
 
-    private RectTransform _rectTransform;
+    private Animator _animator;
+
+    public bool IsFrontSided => _frontSide.activeSelf;
+
+    private void Start()
+    {
+        _animator = GetComponent<Animator>();
+    }
 
     public void Init(CardDeckView parentDeckView, CardScriptableObject card, int playerLevel = 1)
     {
         _card = card;
-        _rectTransform = gameObject.GetComponent<RectTransform>();
 
         _numberText.text = _card.Number.ToString();
         _rewardTypeText.text = parentDeckView.GetAllowedRewards().GetRewardName(card.RewardType);
@@ -26,5 +36,29 @@ public class CardView : MonoBehaviour
     {
         string quantityText = _card.GetRewardQuantity(playerLevel).ToString();
         _rewardQuantityType.text = quantityText;
+    }
+
+    public void ChangeSide()
+    {
+        _frontSide.gameObject.SetActive(!_frontSide.gameObject.activeSelf);
+        _backSide.gameObject.SetActive(!_backSide.gameObject.activeSelf);
+    }
+
+    public void FlipToBackSide()
+    {
+        _animator.SetTrigger("FlipToBack");
+    }
+
+    public void FlipToFrontSide()
+    {
+        _animator.SetTrigger("FlipToFront");
+    }
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        if (!IsFrontSided)
+        {
+            FlipToFrontSide();
+        }
     }
 }
